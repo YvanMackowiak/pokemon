b65bc1c1fb99b5194934a52593cc0910ac8350a6
YvanMackowiak <yvanmackowiak@gmail.com>
2024-11-06 20:12:14 +0100

    sup com

--- frontend/src/app/pokemon/[id]/page.tsx
+++ frontend/src/app/pokemon/[id]/page.tsx
@@ -1,13 +1,11 @@
 import Box from "@mui/material/Box/Box";
 import Typography from "@mui/material/Typography/Typography";
 
-// Cette fonction récupère l'ID depuis les paramètres de l'URL
 export default async function PokemonDetailPage({
   params,
 }: {
   params: { id: string };
 }) {
-  // Utilisez await pour récupérer l'id de manière asynchrone
   const { id } = await params;
 
   return (
@@ -18,9 +16,7 @@ export default async function PokemonDetailPage({
         alignItems: "center",
       }}
     >
-      {/* Contenu de la page avec l'ID du Pokémon */}
       <Typography variant="body1">Détails du Pokémon N°{id}</Typography>
-      {/* Ici vous pouvez ajouter plus de contenu */}
     </Box>
   );
 }
