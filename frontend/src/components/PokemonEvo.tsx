a2b3d3fb5fb237b86c5b7264a92359d8a53fe7dc
YvanMackowiak <yvanmackowiak@gmail.com>
2024-11-17 01:25:35 +0100

    onClick composant evolution

--- frontend/src/components/PokemonEvo.tsx
+++ frontend/src/components/PokemonEvo.tsx
@@ -1,6 +1,8 @@
 import { PokemonEvolutionProps } from "@/hooks/usePokemon";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { Box } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface PokemonEvoProps {
   sprit: string;
@@ -8,17 +10,27 @@ interface PokemonEvoProps {
 }
 
 export const PokemonEvo = ({ sprit, spritEvo }: PokemonEvoProps) => {
+  const router = useRouter();
+
+  const handleClick = (id: number) => {
+    router.push(`/pokemon/${id}`);
+  };
+
   return (
-    <Box display="flex" gap={1}>
+    <Box display="flex" gap={2} alignItems="center">
       {spritEvo?.pre?.map((img) => (
-        <Image
-          key={img.pokedex_id}
-          src={img.sprites.regular}
-          alt="pikapika"
-          width={200}
-          height={200}
-          style={{ objectFit: "contain" }}
-        />
+        <>
+          <Image
+            onClick={() => handleClick(img.pokedex_id)}
+            key={img.pokedex_id}
+            src={img.sprites.regular}
+            alt="pikapika"
+            width={200}
+            height={200}
+            style={{ objectFit: "contain", cursor: "pointer" }}
+          />
+          <ArrowForwardIosIcon />
+        </>
       ))}
       <Image
         src={sprit}
@@ -28,14 +40,18 @@ export const PokemonEvo = ({ sprit, spritEvo }: PokemonEvoProps) => {
         style={{ objectFit: "contain" }}
       />
       {spritEvo?.next?.map((img) => (
-        <Image
-          key={img.pokedex_id}
-          src={img.sprites.regular}
-          alt="pikapika"
-          width={200}
-          height={200}
-          style={{ objectFit: "contain" }}
-        />
+        <>
+          <ArrowForwardIosIcon />
+          <Image
+            onClick={() => handleClick(img.pokedex_id)}
+            key={img.pokedex_id}
+            src={img.sprites.regular}
+            alt="pikapika"
+            width={200}
+            height={200}
+            style={{ objectFit: "contain", cursor: "pointer" }}
+          />
+        </>
       ))}
     </Box>
   );
